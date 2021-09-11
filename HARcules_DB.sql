97c9f1e4d517baae77572128ca55253fc5fd4436
AggelosDiama <aggelosdiama@gmail.com>
2021-09-11 17:17:35 +0300

    okay

---
+++ HARcules_DB.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS HARcules_database; 
+
+USE HARcules_database;
+
+CREATE TABLE IF NOT EXISTS users (
+    
+);
+
+CREATE TABLE IF NOT EXISTS har_files (
+
+);
+
+CREATE TABLE IF NOT EXISTS entries (
+
+);
+
+CREATE TABLE IF NOT EXISTS response (
+
+);
+
+CREATE TABLE IF NOT EXISTS request (
+
+);
